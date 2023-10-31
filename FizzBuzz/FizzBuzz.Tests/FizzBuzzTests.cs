c897db7aaf7a8466fa14dbba69c755bc4e013acd
Maral <mrlmosafercomputer@gmail.com>
2023-10-31 17:42:49 +0330

    Finish RoundsLenght Part

---
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Should_ReturnAListWithGivenRoundsLenght()
+        {
+            //arrange => setup
+            const int rounds = 100; //1-100
+            var fizzBuzz = new FizzBuzz();
+
+            //act => exercise
+            var actual = fizzBuzz.Start(rounds);
+
+            //assert => verify
+            actual.Count.Should().Be(rounds);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public List<string> Start(int rounds)
+        {
+            var result= new List<string>();
+            for (int i = 1; i <= rounds; i++)
+            {
+                result.Add(i.ToString());
+            }
+            return result;
+        }
+    }
+}
