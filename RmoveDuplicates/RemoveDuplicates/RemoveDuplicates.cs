42f2e92b3b7917822eb883dd59cfeb117468cc1a
Maral <mrlmosafercomputer@gmail.com>
2023-10-31 18:33:59 +0330

    RemoveDuplicatesFromList Finish

---
+++ RmoveDuplicates/RemoveDuplicates/RemoveDuplicates.cs
@@ -0,0 +1,11 @@
+﻿namespace RemoveDuplicates
+{
+    public class RemoveDuplicates
+    {
+        public List<int> RemoveDuplicatesFromList(List<int> input)
+        {
+            HashSet<int> uniqueNumbers = new HashSet<int>(input);
+            return uniqueNumbers.ToList();
+        }
+    }
+}
---
+++ RmoveDuplicates/RmoveDuplicate.Tests/RemoveDuplicatesTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+namespace RmoveDuplicate.Tests
+{
+    public class RemoveDuplicatesTests
+    {
+        [Fact]
+        public void Should_ReturnsCorrectResultWithRemoveDuplicates()
+        {
+            //Arrange
+            List<int> input = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+            List<int> expected = new List<int> { 1, 2, 3, 4, 5 };
+
+            //Act
+            var removeDuplicates = new RemoveDuplicates.RemoveDuplicates();
+            var actual = removeDuplicates.RemoveDuplicatesFromList(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
+            //actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
